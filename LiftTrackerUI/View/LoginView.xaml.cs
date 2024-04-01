83ee1d7495beba4411b8d6de8415a95e4af7037e
Marek Lassak <xlassak@fi.muni.cz>
2024-04-01 20:01:08 +0200

    feat: added login window

---
+++ LiftTrackerUI/View/LoginView.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+
+namespace LiftTrackerUI.View
+{
+    public partial class LoginView : Window
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == System.Windows.Input.MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void minimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void loginButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
